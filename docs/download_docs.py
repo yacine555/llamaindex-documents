651e2e8e99ebf1615f516f968038dba7a26a7ab9
Yacine <ybouakkaz@gmail.com>
2024-01-24 22:53:32 -0500

    improve document ingestion

--- docs/download_docs.py
+++ docs/download_docs.py
@@ -3,36 +3,78 @@ from bs4 import BeautifulSoup
 import os
 import urllib
 
-# The URL to scrape
-url = "https://docs.llamaindex.ai/en/stable/"
 
-# The directory to store files in
-output_dir = "./docs/llamindex-docs/"
+def download_site(url:str,output_dir:str,html_filter:str=None):
+    print(f"Downlad site {url} in {output_dir}")
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
 
-# Create the output directory if it doesn't exist
-os.makedirs(output_dir, exist_ok=True)
+    # Fetch the page
+    response = requests.get(url)
 
-# Fetch the page
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
 
-# Find all links to .html files
-links = soup.find_all("a", href=True)
+    # Find all links to .html files
+    links = soup.find_all("a", href=True)
+    print(f"{len(links)} links found")
 
-for link in links:
-    href = link["href"]
+    #create list of pages to avoid duplicate
+    page_download= []
 
-    # If it's a .html file
-    if href.endswith(".html"):
-        # Make a full URL if necessary
-        if not href.startswith("http"):
-            href = urllib.parse.urljoin(url, href)
+    for link in links:
+        href = link["href"]
+        href_abs = ""
+        href_rel = ""
+        print(f"link to download {href}")
 
-        # Fetch the .html file
-        print(f"downloading {href}")
-        file_response = requests.get(href)
+        # If it's a .html file
+        if href.endswith(".html") or href.endswith("/"):
+            page_name = href
+            if href.endswith("/"):
+                page_name = href + "index.html"
 
-        # Write it to a file
-        file_name = os.path.join(output_dir, os.path.basename(href))
-        with open(file_name, "w", encoding="utf-8") as file:
-            file.write(file_response.text)
+            # Make a full URL if necessary
+            if not href.startswith("http"):
+                href = urllib.parse.urljoin(url, href)
+
+            if href not in page_download:
+                page_download.append(href)
+
+                # Fetch the .html file
+                print(f"downloading {href} into {page_name}...")
+                response = requests.get(href)
+                response_text = ""
+                
+
+                if html_filter is not None:
+                    soup = BeautifulSoup(response.text, "html.parser")
+                    list_items = soup.select(html_filter)
+                    for item in list_items:
+                        response_text = response_text + str(item)
+                else:
+                    response_text = response.text
+
+                # print(f"response code {response_text}")
+
+                # Write it to a file
+                
+                file_name = os.path.join(output_dir, os.path.basename(page_name))
+                directory = output_dir + href.replace(url, "").replace("https://","/")
+                file_name = output_dir + page_name.replace("https://","/")
+                
+                if not os.path.exists(directory):
+                    print(f"create directory {directory} ")
+                    os.makedirs(directory)
+
+                print(f"save file into {file_name}")
+                with open(file_name, "w", encoding="utf-8") as file:
+                    file.write(response_text)
+
+            else:
+                print(f"Page {href} already downloaded.")
+
+
+if __name__ == "__main__":
+
+    # download_site("https://docs.llamaindex.ai/en/stable/","./docs/llamindex-docs/")
+    download_site("https://mistral.ai","./docs/mistral_ai/")
